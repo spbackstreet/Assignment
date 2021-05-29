7c361e5b35d949f638131580fa3964b29f65a5bd
sameer.s.patkar <sameer.s.patkar@ril.com>
2021-05-29 12:44:25 +0530

    one more method to find anagram is added

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+//cloning object
 
 const deepClone=(object)=>{
 let copy={...object}
@@ -9,7 +10,7 @@ return (copy)
 deepClone({ name: "Hitachi MGRM NET", address: { city: "Gurgaon", country: "India" } })
 
 
-
+//Find Anagrams
 
 const anagrams=(stringA, stringB)=> {
     console.log(cleanString(stringA) === cleanString(stringB))
@@ -21,3 +22,21 @@ const cleanString=(str)=> {
 }
 
 anagrams('fried', 'fired')
+
+
+
+//Find Anagrams
+
+const findAnagrams=(p1,p2)=>{
+let arr1=p1.split('').sort()
+let arr2=p2.split('').sort()
+
+if(JSON.stringify(arr1)===JSON.stringify(arr2)){
+    console.log('is anagrams')
+}
+else{
+    console.log('not anagrams')
+}
+}
+
+findAnagrams('fried', 'fired')
